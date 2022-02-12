1f949c8870ded8e502a6a52b212d697700cde400
miftahapriliandi <miftahapriliandi@gmail.com>
2022-02-13 06:13:39 +0700

    Splash Screen

---
+++ lib/shared/theme.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color kPrimaryColor = Color(0xFF5C40CC);
+Color kBlackColor = Color(0xFF1F1449);
+Color kWhiteColor = Color(0xFFFFFFFF);
+Color kGreyColor = Color(0xFF9698A9);
+Color kGreenColor = Color(0xFF0EC3AE);
+Color kRedColor = Color(0xFFEB70A5);
+Color kBackgroundColor = Color(0xFFFAFAFA);
+Color kInactiveColor = Color(0xFFDBD7EC);
+
+TextStyle blackTextStyle = GoogleFonts.poppins(
+  color: kBlackColor,
+);
+
+TextStyle whiteTextStyle = GoogleFonts.poppins(
+  color: kWhiteColor,
+);
+
+TextStyle greyTextStyle = GoogleFonts.poppins(
+  color: kGreyColor,
+);
+
+TextStyle greenTextStyle = GoogleFonts.poppins(
+  color: kGreenColor,
+);
+
+TextStyle redTextStyle = GoogleFonts.poppins(
+  color: kRedColor,
+);
+
+TextStyle purpleTextStyle = GoogleFonts.poppins(
+  color: kPrimaryColor,
+);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
