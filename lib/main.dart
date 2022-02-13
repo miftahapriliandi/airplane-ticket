1e73a69faa237b391f49929da0f6eb005f249c48
miftahapriliandi <miftahapriliandi@gmail.com>
2022-02-13 08:48:01 +0700

    Sign Up Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:airplane/ui/pages/get_started_page.dart';
+import 'package:airplane/ui/pages/sign_up_page.dart';
 import 'package:airplane/ui/pages/splash_page.dart';
 import 'package:flutter/material.dart';
 import 'ui/pages/splash_page.dart';
@@ -11,7 +13,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashPage(),
+      routes: {
+        '/': (context) => SplashPage(),
+        '/get-started': (context) => GetStartedPage(),
+        '/sign-up': (context) => SignUpPage(),
+      },
     );
   }
 }
